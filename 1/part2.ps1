0f14e8851f98d970d4c49c80ff61544b943dd9f9
Mitsszz <mitchelsimons1@gmail.com>
2021-12-01 10:51:34 +0100

    Create part2.ps1

---
+++ 1/part2.ps1
@@ -0,0 +1,12 @@
+﻿$input = get-content .\input.txt
+$count = 0
+
+for ($i = 3; $i -lt $input.Count; $i++) {
+    
+    $now = [int]$input[$i] + [int]$input[$i-1] + [int]$input[$i-2]
+    $prev = [int]$input[$i-1] + [int]$input[$i-2] + [int]$input[$i-3]
+    if ($now -gt $prev) {
+        $count++
+    }
+}
+$count
